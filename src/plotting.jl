4e7e1a5d88a74432238a2753c5c9c287b57368a4
Stefanos Carlström <stefanos.carlstrom@fysik.lth.se>
2016-03-09 15:51:29 +0100

    Tick-related convenience functions

--- src/plotting.jl
+++ src/plotting.jl
@@ -42,7 +42,24 @@ function set_times_new_roman(; kwargs...)
        fontset = "stix")
 end
 
-export colormaps, plot_map, set_times_new_roman
-export colormaps, plot_map, set_font, set_times_new_roman
+function latex_base10(v)
+    if v==0
+        return "0"
+    end
+    ex = floor(log10(v))
+    v /= 10^ex
+    @sprintf("%0.3f\\cdot10^{%i}",v,ex)
+end
+
+function axis_add_ticks(ticks, labels, ax = :x)
+    a = gca()
+    a2 = a[ax == :x ? :twiny : :twinx]()
+    a2[ax == :x ? :set_xlim : :set_ylim](a[ax == :x ? :get_xlim : :get_ylim]())
+    a2[ax == :x ? :set_xticks : :set_yticks](collect(ticks))
+    a2[ax == :x ? :set_xticklabels : :set_yticklabels](collect(labels))
+    sca(a)
+end
+
+export colormaps, plot_map, set_font, set_times_new_roman, latex_base10, axis_add_ticks
 
 end
